07ca462444c18f4a95c3b39799d180d755f9b4fd
Yousef Ahmed <115221628+youssef-ahmmed@users.noreply.github.com>
2025-04-04 00:00:51 +0200

    Add project setup & configurations

---
+++ src/test/java/me/sideproject/urlshorten/UrlShortenApplicationTests.java
@@ -0,0 +1,13 @@
+package me.sideproject.urlshorten;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortenApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
