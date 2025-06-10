2fd56ed8b7b8626bb2b479d7bf7edb0cfa21dc39
Yousef Ahmed <115221628+youssef-ahmmed@users.noreply.github.com>
2025-06-10 09:30:57 +0300

    Fix @param import to use from spring not redis

--- src/main/java/me/sideproject/urlshortener/repository/URLMapRepository.java
+++ src/main/java/me/sideproject/urlshortener/repository/URLMapRepository.java
@@ -1,9 +1,9 @@
 package me.sideproject.urlshortener.repository;
 
-import io.lettuce.core.dynamic.annotation.Param;
 import me.sideproject.urlshortener.models.URLMap;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
